738f3a1930bb5f807ac637fd8e9806b551831896
wortback <wortback.dev@gmail.com>
2025-06-30 11:00:16 +0200

    Runtime Debug Sustem
    Detailed Description:
    Added the backbone of the runtime debug system. It is supposed to test DebugManager and DialogueAssets. Is contains two widgets and a DebugController. Debug Controller is spawned by the DialogueUIManager and owns the widgets. DialogueManager sends the UI manager a render request, and the UI manager then redirects the rendering part to the DebugController.

--- Source/DialoguePlugin/DialoguePlugin.Build.cs
+++ Source/DialoguePlugin/DialoguePlugin.Build.cs
@@ -39,6 +39,7 @@ public class DialoguePlugin : ModuleRules
                 "Engine",
                 "Slate",
                 "SlateCore",
+                "UMG",
 				// ... add private dependencies that you statically link with here ...	
 			}
             );
