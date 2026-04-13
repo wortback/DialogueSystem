989f0b8b52064c949c89233534408463e87cc930
wortback <wortback.dev@gmail.com>
2026-04-13 15:30:00 +0200

    [Refac: Production Support] Detailed Description: 1. Added a way to display dialogue UI through a non-debug way (managed by IDialogueDisplayResolver) 2. Fixed the hard ref to DialogueUIManager in DialogueManager.

---
+++ Source/DialoguePlugin/Private/Runtime/DialogueDisplayResolver.cpp
@@ -0,0 +1,7 @@
+﻿// Copyright © 2026 created and maintained by wortback, Ellufin Soft. All rights reserved.
+
+
+#include "Runtime/DialogueDisplayResolver.h"
+
+
+// Add default functionality here for any IDialogueDisplayResolver functions that are not pure virtual.
---
+++ Source/DialoguePlugin/Public/Runtime/DialogueDisplayResolver.h
@@ -0,0 +1,37 @@
+﻿// Copyright © 2026 created and maintained by wortback, Ellufin Soft. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Runtime/DialogueRuntimeTypes.h"
+#include "DialogueDisplayResolver.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UDialogueDisplayResolver : public UInterface {
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class DIALOGUEPLUGIN_API IDialogueDisplayResolver {
+	GENERATED_BODY()
+
+public:
+	/** Display a dialogue sentence (NPC speaking) */
+	virtual void DisplaySentence(const FDisplayObject& Obj) = 0;
+    
+	/** Display dialogue choices for player selection */
+	virtual void DisplayChoice(const FDisplayObject& Obj) = 0;
+    
+	/** Get whether this resolver is active/enabled */
+	virtual bool IsActiveResolver() const = 0;
+	
+	/** Run at the beginning of dialogue */
+	virtual void StartDialogue() {};
+	
+	/** Run at the end of dialogue */
+	virtual void EndDialogue() {};
+};
